98d963563e82af50443ab597f70cc291c7b97c37
Golmoradi Majid (Electronic Banking Services) <M.Golmoradi@mebank.com>
2022-10-08 14:29:05 +0330

    add splash screen

--- app/src/main/java/com/mgm/a1ctest/ui/MainActivity.kt
+++ app/src/main/java/com/mgm/a1ctest/ui/MainActivity.kt
@@ -2,15 +2,28 @@ package com.mgm.a1ctest.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.mgm.a1ctest.R
 import com.mgm.a1ctest.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     //Binding
     private lateinit var binding: ActivityMainBinding
+    //Navigation
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        //InitViews
+        binding.apply {
+            //Navigation
+            navController = findNavController(R.id.navHost)
+        }
     }
 }
--- app/src/main/java/com/mgm/a1ctest/ui/splash/SplashFragment.kt
+++ app/src/main/java/com/mgm/a1ctest/ui/splash/SplashFragment.kt
@@ -5,7 +5,11 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.coroutineScope
+import androidx.navigation.fragment.findNavController
+import com.mgm.a1ctest.R
 import com.mgm.a1ctest.databinding.FragmentSplashBinding
+import kotlinx.coroutines.delay
 
 class SplashFragment : Fragment() {
     //Binding
@@ -17,6 +21,16 @@ class SplashFragment : Fragment() {
     ): View {
         binding = FragmentSplashBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        //set delay
+        lifecycle.coroutineScope.launchWhenCreated {
+            delay(1000)
+            //Navigate to manufacturer after 1 sec
+            findNavController().navigate(R.id.action_splashFragment_to_manufacturerFragment)
+        }
     }
 
 }
